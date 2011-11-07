5a283991e1a7ba1d00f8148405a71934b775525d
Chris Jerdonek <chris.jerdonek@gmail.com>
2011-11-07 00:37:37 -0800

    First commit.

---
+++ lib/parser.py
@@ -0,0 +1,222 @@
+# encoding: utf-8
+#
+# Copyright (C) 2011 Chris Jerdonek.  All rights reserved.
+#
+
+import logging
+import os
+import sys
+
+_log = logging.getLogger(__name__)
+
+
+class Error(Exception):
+    """Base class for exceptions in this module."""
+    pass
+
+
+class Contest(object):
+
+    def __init__(self, name, id, candidate_dict, winner_id, other_finalist_ids):
+
+        candidate_ids = candidate_dict.keys()
+        finalist_ids = [winner_id] + other_finalist_ids
+
+        self.id = id
+        self.name = name
+        self.winner_id = winner_id
+        self.candidate_dict = candidate_dict
+
+        self.candidate_ids = candidate_ids
+        self.other_finalist_ids = other_finalist_ids
+        self.finalist_ids = finalist_ids
+        self.non_finalist_ids = list(set(candidate_ids) - set(finalist_ids))
+
+
+class MasterParser(object):
+
+    def __init__(self, winning_candidate, final_candidates):
+        self.winning_candidate = winning_candidate
+        self.final_candidates = final_candidates
+
+    def parse(self, path):
+        _log.info("Reading master file: %s" % path)
+        with open(path, "r") as f:
+            contest = self.read_master_file(f)
+
+        return contest
+
+    def find_candidate_id(self, candidate_dict, name_to_find):
+        for (candidate_id, name) in candidate_dict.iteritems():
+            if name == name_to_find:
+                return candidate_id
+        raise Error("Candidate %s not found in dictionary." % name_to_find)
+
+    def read_master_file(self, f):
+        candidate_dict = {}
+
+        while True:
+            line = f.readline()
+            if not line:
+                break
+
+            record_type, record_id, description = self.parse_master_line(line)
+
+            if record_type == "Contest":
+                contest_id = record_id
+                contest_name = description
+                continue
+
+            if record_type == "Candidate":
+                candidate_dict[record_id] = description
+
+        winner_id = self.find_candidate_id(candidate_dict, self.winning_candidate)
+
+        finalist_ids = []
+        for candidate in self.final_candidates:
+            finalist_id = self.find_candidate_id(candidate_dict, candidate)
+            finalist_ids.append(finalist_id)
+
+        contest = Contest(contest_name, contest_id, candidate_dict, winner_id, finalist_ids)
+
+        return contest
+
+    def parse_master_line(self, line):
+        """
+        Parse the line, and return a tuple.
+
+        A sample line--
+
+        "Candidate 0000120JANET REILLY                                      0000001000000700"
+
+        """
+        # We only care about the first three fields: Record_Type, Id, and Description.
+        record_type = line[0:10].strip()
+        id = int(line[10:17])
+        description = line[17:67].strip()
+
+        return record_type, id, description
+
+
+
+class BallotParser(object):
+    
+    def __init__(self, undervote, overvote, on_ballot):
+        self.undervote = undervote
+        self.overvote = overvote
+        self.on_ballot = on_ballot
+
+    def process_contest(self, ballot_path, winner):
+        self.read_ballot_path(ballot_path)
+
+    def read_ballot_path(self, path):
+        _log.info("Reading ballots: %s" % path)
+        with open(path, "r") as f:
+            self.process_ballot_file(f)
+
+    def process_ballot_file(self, f):
+
+        line = f.readline()
+        parsed_line = self.parse_line(line)
+        contest_id = parsed_line[0]
+
+        line_index = 1
+
+        try:
+            while True:
+                ballot = self.read_ballot(parsed_line, f)
+
+                self.on_ballot(ballot)
+
+                line = f.readline()
+                if not line:
+                    break
+                line_index += 3
+
+                parsed_line = self.parse_line(line)
+                if parsed_line[0] != contest_id:
+                    raise Exception("Expected contest id: %s" % expected_contest_id)
+
+        except Error as e:
+            _log.error("Error processing ballot beginning at line %s" % line_index)
+            raise e
+
+
+    def read_ballot(self, parsed_line, f):
+        """
+        Read and return an RCV ballot.
+
+        Arguments:
+
+          parsed_line: a tuple that is the first line of an RCV ballot.  The
+            caller is responsible for confirming that the contest ID is correct.
+
+          f: a file handle.
+
+        Returns:
+
+          a 3-tuple of integers representing the choices on an RCV ballot.
+          Each integer is a candidate ID, -1 for undervote, or -2 for overvote.
+
+        """
+        contest_id, voter_id, rank, choice = parsed_line
+
+        if rank != 1:
+            raise Error("Expected rank: 1")
+
+        choices = [choice]
+
+        line = f.readline()
+        parsed_line = self.parse_ballot_line(contest_id, voter_id, 2, line)
+        choices.append(parsed_line[3])
+
+        line = f.readline()
+        parsed_line = self.parse_ballot_line(contest_id, voter_id, 3, line)
+        choices.append(parsed_line[3])
+
+        return choices
+
+
+    def parse_ballot_line(self, expected_contest_id, expected_voter_id, expected_rank, line):
+        """
+        Return a parsed line, or raise an Exception on failure.
+
+        """
+        parsed_line = self.parse_line(line)
+
+        if parsed_line[0] != expected_contest_id:
+            raise Error("Expected contest id: %s" % expected_contest_id)
+        if parsed_line[1] != expected_voter_id:
+            raise Error("Expected voter id: %s" % expected_voter_id)
+        if parsed_line[2] != expected_rank:
+            raise Error("Expected rank: %s" % expected_rank)
+
+        return parsed_line
+
+    def parse_line(self, line):
+        """
+        Return a parsed line as a tuple.
+
+        A sample input--
+
+        000000700001712400000090020000331001000012600
+
+        The corresponding return value--
+
+
+
+        """
+        # TODO: consider having this function return an object.
+        contest_id = int(line[0:7])
+        voter_id = int(line[7:16])
+        rank = int(line[33:36])
+        candidate_id = int(line[36:43])
+        undervote = self.undervote if int(line[44]) else 0
+        overvote = self.overvote if int(line[43]) else 0
+
+        choice = candidate_id or undervote or overvote
+
+        return (contest_id, voter_id, rank, choice)
+
+
+
---
+++ lib/reporter.py
@@ -0,0 +1,275 @@
+# encoding: utf-8
+#
+# Copyright (C) 2011 Chris Jerdonek.  All rights reserved.
+#
+
+import logging
+import os
+import sys
+
+_log = logging.getLogger(__name__)
+
+
+LABELS = {
+    'total': "Total",
+    'voted': "Voted",
+    'under': "Undervoted",
+    'has_dupe': "Has_Duplicate",
+    'has_over': "Has_Overvote",
+    'has_skip': "Has_Skipped",
+    'irregular': "Irregular",
+    'over': "Overvoted",
+    'continuing': "Continuing",
+    'valid': "Valid",
+    'all': "All",
+    'winner': "Winner",
+    'finalists': "Finalists",
+    'non-finalists': "Non-finalists",
+}
+
+
+def percent(part, whole):
+    """
+    Return a percent float.
+
+    """
+    return 100.0 * part / whole
+
+def ljust(s, length, fill_string):
+    """
+    Right pad a string with a fill string, for example "ALICE . . . .".
+    """
+    fill_length = len(fill_string)
+    while len(s) < length:
+        # Rotate through the characters in the fill string.
+        s += fill_string[len(s) % fill_length]
+
+    return s
+
+class Reporter(object):
+
+    candidate_indent = 12
+
+    def __init__(self, contest, stats, download_url, download_time):
+        self.contest = contest
+        self.stats = stats
+        self.download_url = download_url
+        self.download_time = download_time
+        
+        self.report = ""
+
+        labels = LABELS.values() + contest.candidate_dict.values()
+        max_label_length = max([len(label) for label in labels])
+        self.left_indent = max_label_length + 1  # for extra space.
+
+        # Sort candidates in descending order by first-round totals.
+        triples = []
+        for candidate_id, name in contest.candidate_dict.iteritems():
+            first_round = stats.get_first_round(candidate_id)
+            triples.append((first_round, candidate_id, name))
+        triples.sort()
+        triples.reverse()
+        self.sorted_candidates = [(triple[1], triple[2], triple[0]) for triple in triples]
+
+    def percent_string(self, part, whole):
+        """
+        Return a percent string, for example, " 12.5%".
+
+        """
+        return "%5.1f%%" % percent(part, whole)
+
+    def rounded_percent_string(self, part, whole):
+        """
+        Return a rounded percent string, for example, " 25%".
+
+        """
+        return "%3.0f%%" % percent(part, whole)
+
+    def get_candidate_name(self, candidate_id):
+        return self.contest.candidate_dict[candidate_id]
+
+    def add_text(self, text):
+        self.report += "%s\n" % text
+
+    def _add_header(self, text, sep_char):
+        self.add_text(text)
+        self.add_text(len(text) * sep_char)
+        self.skip()
+
+    def add_title(self, text):
+        self._add_header(text, "=")
+
+    def add_section_title(self, text):
+        self.skip()
+        self._add_header(text, "-")
+
+    def _add_candidate_name(self, candidate_id):
+        name = self.get_candidate_name(candidate_id)
+        self.add_text(3 * " " + name)
+
+    def add_candidate_names(self, header, candidate_ids):
+        self.add_text("%s:" % header)
+        for candidate_id in candidate_ids:
+            self._add_candidate_name(candidate_id)
+        self.skip()
+
+    def label_string(self, name):
+        s = "%s " % name.upper()
+        s = ljust(s, self.left_indent, '.')
+
+        return s
+
+    def value_string(self, value):
+        return "%6d" % value
+
+    def add_data(self, name, value, total_label=None, total=None):
+        label_string = self.label_string(name)
+
+        percent = self.percent_string(value, total)
+        value_string = self.value_string(value)
+
+        s = "%s %s (%s of %s)" % (label_string, value_string, percent, total_label)
+
+        self.add_text(s)
+
+    def add_number_ranked(self, name, number_ranked):
+        first_round = sum(number_ranked)
+
+        label_string = self.label_string(name)
+        percent_strings = [self.rounded_percent_string(value, first_round) for value in number_ranked]
+        number_ranked_strings = [str(value) for value in number_ranked]
+
+        strings = [label_string] + percent_strings + [first_round] + number_ranked_strings
+
+        s = "%s %s %s %s (%s = %s + %s + %s)" % tuple(strings)
+
+        self.add_text(s)
+
+    def add_aggregate_number_ranked(self, name, candidate_ids):
+        number_ranked_list = [self.stats.get_number_ranked(candidate_id) for candidate_id in candidate_ids]
+        number_ranked = map(sum, zip(*number_ranked_list))
+
+        self.add_number_ranked(name, number_ranked)
+
+    def add_percent_data(self, name, value, total):
+        label_string = self.label_string(name)
+        percent_string = self.percent_string(value, total)
+        value_string = self.value_string(value)
+        total_string = self.value_string(total)
+
+        s = "%s %s (%s / %s)" % (label_string, percent_string, value_string, total_string)
+
+        self.add_text(s)
+
+    def add_first_round_percent_data(self, name, value_dict, candidate_ids):
+        value = sum([value_dict[candidate_id] for candidate_id in candidate_ids])
+        total = sum([self.stats.get_first_round(candidate_id) for candidate_id in candidate_ids])
+
+        self.add_percent_data(name, value, total)
+
+    def skip(self):
+        self.report += "\n"
+
+    def make_report(self):
+
+        contest = self.contest
+        stats = self.stats
+
+        self.add_title(contest.name + " RCV Stats")
+
+        self.add_candidate_names(LABELS['winner'], [contest.winner_id])
+        self.add_candidate_names(LABELS['finalists'], contest.finalist_ids)
+
+        self.add_data(LABELS['total'], stats.total, 'total', stats.total)
+        self.skip()
+        self.add_data(LABELS['voted'], stats.voted, 'total', stats.total)
+        self.add_data(LABELS['under'], stats.undervotes, 'total', stats.total)
+        self.skip()
+
+        self.add_data(LABELS['has_dupe'], stats.has_duplicate, 'voted', stats.voted)
+        self.add_data(LABELS['has_over'], stats.has_overvote, 'voted', stats.voted)
+        self.add_data(LABELS['has_skip'], stats.has_skipped, 'voted', stats.voted)
+        self.add_data(LABELS['irregular'], stats.irregular, 'voted', stats.voted)
+
+        self.add_section_title("First round")
+
+        self.add_data(LABELS['continuing'], stats.first_round_continuing, 'voted', stats.voted)
+        self.add_data(LABELS['over'], stats.first_round_overvotes, 'voted', stats.voted)
+        self.skip()
+
+        for candidate_id, name, first_round in self.sorted_candidates:
+            self.add_data(name, first_round, 'continuing', stats.first_round_continuing)
+
+        self.add_section_title("Number of candidates validly ranked (3-2-1), by first round")
+
+        self.add_aggregate_number_ranked(LABELS['all'], contest.candidate_ids)
+        self.add_aggregate_number_ranked(LABELS['winner'], [contest.winner_id])
+        self.add_aggregate_number_ranked(LABELS['finalists'], contest.finalist_ids)
+        self.add_aggregate_number_ranked(LABELS['non-finalists'], contest.non_finalist_ids)
+        self.skip()
+
+        for candidate_id, name, first_round in self.sorted_candidates:
+            number_ranked = stats.get_number_ranked(candidate_id)
+            self.add_number_ranked(name, number_ranked)
+
+        self.add_section_title("Ballots validly ranking the winner, by first round")
+
+        self.add_first_round_percent_data(LABELS['all'], stats.ranked_winner, contest.candidate_ids)
+        self.skip()
+
+        for candidate_id, name, first_round in self.sorted_candidates:
+            self.add_first_round_percent_data(name, stats.ranked_winner, [candidate_id])
+
+        self.add_section_title("Ballots validly ranking a finalist, by first round")
+
+        self.add_first_round_percent_data(LABELS['all'], stats.ranked_finalist, contest.candidate_ids)
+        self.skip()
+
+        for candidate_id, name, first_round in self.sorted_candidates:
+            self.add_first_round_percent_data(name, stats.ranked_finalist, [candidate_id])
+
+        self.add_section_title("Ballots ranking the same candidate 3 times")
+
+        self.add_first_round_percent_data(LABELS['all'], stats.did_sweep, contest.candidate_ids)
+        self.skip()
+
+        for candidate_id, name, first_round in self.sorted_candidates:
+            self.add_first_round_percent_data(name, stats.did_sweep, [candidate_id])
+
+        self.add_section_title("Condorcet support for winner against each candidate, in ascending order")
+
+        # TODO: move the condorcet code below into a method.
+        condorcet_data = []
+        for candidate_id, name, first_round in self.sorted_candidates:
+            if candidate_id == contest.winner_id:
+                continue
+            win_count, total_count = stats.get_condorcet_support(contest.winner_id, candidate_id)
+            new_data = (percent(win_count, total_count), win_count, total_count, name)
+            condorcet_data.append(new_data)
+        condorcet_data.sort()
+
+        for data in condorcet_data:
+            # BOB  60% (600 / 1000 = 20% of first-round continuing)
+            percent_value, win_count, total_count, name = data
+            
+            label_string = self.label_string(name)
+            percent_string = self.percent_string(win_count, total_count)
+            percent_of_voted_string = self.percent_string(total_count, stats.first_round_continuing)
+
+            strings = [label_string, percent_string, win_count, total_count, percent_of_voted_string]
+
+            s = "%s %s (%s / %s) (%s of first-round continuing)" % tuple(strings)
+
+            self.add_text(s)
+
+        
+
+        self.skip()
+        self.add_text("[Data downloaded from %s" % self.download_url)
+        self.add_text(" on %s.]" % self.download_time)
+        self.skip()
+        self.skip()
+
+        return self.report
+
+
